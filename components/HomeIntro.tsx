7c387aebe34ddb3af6ad5b888979de2e51ffeb0c
madrastrailsin-byte <madrastrails.in@gmail.com>
2026-08-08 11:40:12 +0530

    Restore homepage intro on mobile

--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -1,22 +1,11 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
 
 const HomeIntroDesktop = dynamic(() => import('./HomeIntroDesktop'), {
   ssr: false,
 })
 
 export default function HomeIntro() {
-  const [showDesktopIntro, setShowDesktopIntro] = useState(false)
-
-  useEffect(() => {
-    if (window.matchMedia('(hover: hover) and (pointer: fine)').matches) {
-  setShowDesktopIntro(true)
-}
-  }, [])
-
-  if (!showDesktopIntro) return null
-
   return <HomeIntroDesktop />
 }
